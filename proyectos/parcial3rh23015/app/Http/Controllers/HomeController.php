66a1506f9bbbf8933a7ec7b27ae9b778f8d8e935
W4lterRomero <rh23015@ues.edu.sv>
2025-11-07 11:57:08 -0600

    Ejercicio1Procesos

--- proyectos/parcial3rh23015/app/Http/Controllers/HomeController.php
+++ proyectos/parcial3rh23015/app/Http/Controllers/HomeController.php
@@ -7,7 +7,41 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     //
-    public function ejercicio1(){
+    public function ejercicio1(Request $request){
         return view('pages.ejercicio1');
+    }
+
+    public function calculos(Request $request){
+
+        $request->validate([
+            'capital'=>'required|numeric',
+            'tasa'=> 'required|numeric',
+            'cuota'=>'required|numeric'
+        ]);
+
+        $capital = $request['capital'];
+        $tasa = $request['tasa'] / 100;
+        $cuota = $request['cuota'];
+
+        $P = $cuota * ((1 - pow(1 + $tasa, -$capital)) / $tasa);
+
+        $tabla = [];
+        $saldo = $P;
+
+        for ($i = 1; $i <= $capital; $i++) {
+            $interes = $saldo * $tasa;
+            $principal = $cuota - $interes;
+            $saldo = $saldo - $principal;
+
+            $tabla[] = [
+                'periodo' => $i,
+                'cuota' => $cuota,
+                'interes' => $interes,
+                'principal' => $principal,
+                'saldo' => max(0, $saldo)
+            ];
+        }
+
+        return view('pages.resultado', compact('tabla', 'P'));
     }
 }
--- proyectos/parcial3rh23015/resources/views/pages/ejercicio1.blade.php
+++ proyectos/parcial3rh23015/resources/views/pages/ejercicio1.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 
+@if ($errors->any())
+    <h3>Por favor corrija los siguientes errores</h3>
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
 <form class ="form form-control" action="{{ route('ejercicio1') }}" method="get">
 
 <label for="">capital</label>
