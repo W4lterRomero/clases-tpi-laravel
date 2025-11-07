f20e4d97b46f3103371d70965db1f1cfb2e90cc9
W4lterRomero <rh23015@ues.edu.sv>
2025-11-07 11:49:14 -0600

    Inicio del ejercicio1

--- proyectos/parcial3rh23015/resources/views/pages/ejercicio1.blade.php
+++ proyectos/parcial3rh23015/resources/views/pages/ejercicio1.blade.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.layout')
 
-<form action="{{ route('ejercicio1') }}" method="get"></form>
-</body>
-</html>
+@section('content')
+
+<form class ="form form-control" action="{{ route('ejercicio1') }}" method="get">
+
+<label for="">capital</label>
+<input type="text" name="capital">
+<label for="">Intereses y mensaul</label>
+<input type="text" name="tasa">
+<label for="">Cuotas y plazos</label>
+<input type="text" name="cuota">
+<button class="btn btn-primary" type="submit">Enviar</button>
+</form>
+@endsection
